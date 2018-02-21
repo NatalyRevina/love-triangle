b73ad6efad6f2d50da264147ae201d4a792ab8bb
Revina Nataliia <natalyrevina@gmail.com>
2018-02-21 23:46:21 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,28 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+	var count = 0;
+	var a = 0;
+	var ai = 0;
+	var b = 0;
+	var bi = 0;
+	var c = 0;
+	var ci = 0;
+	
+	for ( var i = 0; i <= preferences.length-1; i++){
+		if(preferences[i] > 0 && preferences[i] < preferences.length+1 && preferences[i]!= i+1){
+			a = preferences[i];
+			ai = i+1;
+			b = preferences[a-1];
+			bi = a;
+			c = preferences[b-1];
+			ci = b;
+			
+			if(a==bi&&b==ci&&c==ai){
+				count = count+1;
+			}
+		
+		}
+	}
+	return count/3;
+}
